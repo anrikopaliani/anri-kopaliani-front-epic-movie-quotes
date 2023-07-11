fdb33cbe533c8a44b40ef836fd82d87be97c9476
anrikopaliani <anrikopa@gmail.com>
2023-07-11 15:03:40 +0400

    fix: put components folder in src

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      spacing: {
+        601: "37.563rem",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
