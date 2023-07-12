28dd253637b3e77308c05e53af5a12f772d94e40
anrikopaliani <anrikopa@gmail.com>
2023-07-12 14:42:34 +0400

    feat: invalid icon when input error

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { useFormContext } from "react-hook-form";
 import { InputProps } from "./types";
+import { InvalidIcon } from "@/components";
 
 const Input: FC<InputProps> = ({
   label,
@@ -11,19 +12,26 @@ const Input: FC<InputProps> = ({
 }) => {
   const { register } = useFormContext();
   return (
-    <div className="flex flex-col mt-4">
+    <div className="flex flex-col mt-5">
       <label htmlFor={name}>
         {label} <span className="text-red-500">*</span>
       </label>
-      <input
-        type={type}
-        {...register(name)}
-        placeholder={placeholder}
-        name={name}
-        id={name}
-        className="mt-2 h-9 pl-2 bg-input rounded text-black"
-      />
-      <p>{error}</p>
+      <div>
+        <div className="relative">
+          <input
+            type={type}
+            {...register(name)}
+            placeholder={placeholder}
+            name={name}
+            id={name}
+            className={`mt-2 h-9 w-full pl-2  bg-input rounded ${
+              error ? "border-error border" : ""
+            } text-black`}
+          />
+          {error && <InvalidIcon />}
+        </div>
+        {error && <p className=" absolute text-error text-sm">{error}</p>}
+      </div>
     </div>
   );
 };
