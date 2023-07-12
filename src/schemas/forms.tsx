abc6b60a3fd9360b46a8b0ab4095df8b7ed65f6d
anrikopaliani <anrikopa@gmail.com>
2023-07-12 14:47:47 +0400

    feat:yup validation only lowercase letters

--- src/schemas/forms.tsx
+++ src/schemas/forms.tsx
@@ -4,14 +4,16 @@ export const signUpFormValidation = Yup.object({
   name: Yup.string()
     .required("name is required")
     .min(3, "name should be at least 3 characters long")
-    .max(15, "name shouldn't be more than 15 characters long"),
+    .max(15, "name shouldn't be more than 15 characters long")
+    .lowercase("should be lowercase"),
   email: Yup.string()
     .required("email is required")
     .email("email format is required"),
   password: Yup.string()
     .required("password is required")
     .min(8, "password should be at least 8 characters long")
-    .max(15, "password shouldn't be more than 15 characters long"),
+    .max(15, "password shouldn't be more than 15 characters long")
+    .lowercase("should be lowercase "),
   confirm_password: Yup.string()
     .required("Please retype your password.")
     .oneOf([Yup.ref("password")], "Your passwords do not match."),
