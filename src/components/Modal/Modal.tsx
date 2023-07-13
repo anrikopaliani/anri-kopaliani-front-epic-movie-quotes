ec2f1461406a6e375cabbbae014454009800790d
anrikopaliani <anrikopa@gmail.com>
2023-07-13 23:30:18 +0400

    refactor: modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -2,23 +2,21 @@ import { FC } from "react";
 import { ModalProps } from "./types";
 
 const Modal: FC<ModalProps> = ({ children, isOpen, toggle, width, height }) => {
+  if (!isOpen) return null;
+
   return (
-    <>
-      {isOpen && (
-        <div
-          onClick={toggle}
-          className="w-screen h-screen absolute top-0 flex justify-center items-center"
-        >
-          <div
-            onClick={(e) => e.stopPropagation()}
-            style={{ width, height }}
-            className=" z-40 bg-modalTheme rounded-xl flex flex-col justify-center items-center"
-          >
-            {children}
-          </div>
-        </div>
-      )}
-    </>
+    <div
+      onClick={toggle}
+      className="w-screen h-screen absolute top-0 flex justify-center items-center"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        style={{ width, height }}
+        className=" z-40 bg-modalTheme rounded-xl flex flex-col justify-center items-center"
+      >
+        {children}
+      </div>
+    </div>
   );
 };
 
