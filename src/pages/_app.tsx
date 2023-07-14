2838c1e29806920e9fa39a7fc692ea02a4361702
anrikopaliani <anrikopa@gmail.com>
2023-07-14 21:58:33 +0400

    chore:remove unneccessary  imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,3 @@
-import { Provider } from "react-redux";
-import type { AppProps } from "next/app";
-import { store } from "@/redux/store";
 import "@/styles/globals.css";
 import { AppPropsWithLayout } from "./types";
 
