00990882b326218cf13c639b447e3f1bd03c3f8d
anrikopaliani <anrikopa@gmail.com>
2023-07-15 00:11:46 +0400

    fix: send register post request correctly

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -5,8 +5,10 @@ export const verifyUserEmail = (url: string) => {
   return axios.get(url).then((response) => console.log(response));
 };
 
-export const registerUser = async (data: RegisterDataTypes) => {
-  return await axios.post("/api/register", data);
+export const registerUser = (data: RegisterDataTypes) => {
+  return axios.get("/sanctum/csrf-cookie").then((response) => {
+    axios.post("/api/register", data);
+  });
 };
 
 export const loginUser = (data: LoginDataTypes) => {
